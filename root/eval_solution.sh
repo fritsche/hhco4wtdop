ed235870dffd6cfa1408e63519562731d3c98e3e
Gian Fritsche <gian.fritsche@gmail.com>
2019-10-23 13:48:57 -0300

    script to evaluate solutions

--- root/eval_solution.sh
+++ root/eval_solution.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 
-if [ "$#" -ne 8 ]; then
+# if any error stop!
+set -e
+
+# validate input size
+if [ "$#" -ne 35 ]; then
 	echo -e "Expected <alg> <id> <fe> <v_1> ... <v_32> "
     echo -e "\talg: algorithm name"
     echo -e "\tid: independent run id"
@@ -7,3 +12,33 @@ if [ "$#" -ne 8 ]; then
     echo -e "\tv_i: variable values separated by space"
 	exit 1
 fi
+
+# read input
+alg=$1; shift
+id=$1; shift
+id=$(printf "%03d" $id)
+fe=$1; shift
+fe=$(printf "%04d" $fe)
+vars=( "$@" )
+
+# create algorithm result folder
+folder="root/experiment/"$alg"/interface/work_"$id"th"
+mkdir -p $folder
+
+# create vars file
+echo ${vars[*]} | tr " " "\t" > $folder/pop_vars_eval.txt
+
+# evaluate objs and cons
+source root/jpnsecCompetition2019/bin/activate
+python root/wisdem/windturbine_MOP.py $folder &> $folder"/gen_"$fe"_log.txt"
+
+# output objs and cons
+cat $folder/pop_objs_eval.txt
+cat $folder/pop_cons_eval.txt
+
+# rename output files
+cd $folder
+for file in pop_*; do
+    mv $file "gen_"$fe"_"$file
+done
+cd - &> /dev/null
